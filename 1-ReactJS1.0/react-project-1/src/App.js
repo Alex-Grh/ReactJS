476b15ce5886c69d7e7a8f88415144e0687214e4
Alex-Grh <alex_grh@mail.ru>
2023-01-06 03:39:49 +0400

    1

--- 1-ReactJS1.0/react-project-1/src/App.js
+++ 1-ReactJS1.0/react-project-1/src/App.js
@@ -1,54 +1,18 @@
+import React, { Profiler } from "react";
 // import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 const App = () => {
    return (
-      <div>
+      <div className="app-wrapper">
          <Header />
-        <Technologies />
+         <Navbar />
+         <Profile />
       </div>
    );
-}
-
-const Technologies = () => {
-   return (
-      <div>
-         <ul>
-            <li>html</li>
-            <li>css</li>
-            <li>js</li>
-            <li>react</li>
-         </ul>
-      </div>
-   );
-}
-
-const Header = () => {
-   return (<div>
-      <a href='#s'>Home</a>
-      <a href='#s'>News Feed</a>
-      <a href='#s'>Messages</a>
-   </div>);
 }
 
 export default App;
---
+++ 1-ReactJS1.0/react-project-1/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+   return <header className="header">
+      <img src="https://thelogoworld.com/wp-content/uploads/2019/07/TLW-Logo-Final.png" />
+   </header>
+}
+
+export default Header;
