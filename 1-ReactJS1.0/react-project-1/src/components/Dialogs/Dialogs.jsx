0931d254327c6d6acdf8a5d6a67e9bc36a96a720
Alex-Grh <alex_grh@mail.ru>
2023-01-09 03:42:11 +0400

    Dialog page layout

--- 1-ReactJS1.0/react-project-1/src/components/Dialogs/Dialogs.jsx
+++ 1-ReactJS1.0/react-project-1/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import s1 from './Dialogs.module.css';
 
 const Dialogs = (props) => {
    return (
-      <div>
-         Dialogs
+      <div className={s1.dialogs}>
+         <div className={s1.dialogsItems}>
+            <div className={`${s1.dialog} ${s1.active}`}>
+               Jack
+            </div>
+            <div className={s1.dialog}>
+               Harry
+            </div>
+            <div className={s1.dialog}>
+               Jacob
+            </div>
+            <div className={s1.dialog}>
+               Charley
+            </div>
+            <div className={s1.dialog}>
+               Thomas
+            </div>
+            <div className={s1.dialog}>
+               George
+            </div>
+         </div>
+         <div className={s1.messages}>
+            <div className={s1.message}>Hello</div>
+            <div className={s1.message}>We study here</div>
+            <div className={s1.message}>You do it really well</div>
+         </div>
       </div>
    )
 }
