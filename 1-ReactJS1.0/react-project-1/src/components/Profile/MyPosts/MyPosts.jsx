d8ddfb10b437166b0c15741cc2c4a21e68f09504
Alex-Grh <alex_grh@mail.ru>
2023-01-25 23:42:56 +0400

    28

--- 1-ReactJS1.0/react-project-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ 1-ReactJS1.0/react-project-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,18 +2,10 @@ import React from "react";
 import s1 from './MyPosts.module.css';
 import Post from "./Post/Post";
 
-const MyPosts = () => {
-
-   let posts = [
-      { id: 1, message: 'Hi, how are you?', likesCount: 12 },
-      { id: 2, message: 'It\'s my first post 1', likesCount: 11 },  
-      { id: 3, message: 'It\'s my first post 2', likesCount: 11 },
-      { id: 4, message: 'It\'s my first post 3', likesCount: 11 },
-      { id: 5, message: 'It\'s my first post 4', likesCount: 11 }   
-   ]
+const MyPosts = (props) => {
 
    let postElements = 
-   posts.map( p => <Post message={p.message} likesCount={p.likesCount} />);
+   props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} />);
 
    return (
       <div className={s1.postBlock}>
--- 1-ReactJS1.0/react-project-1/src/components/Profile/Profile.jsx
+++ 1-ReactJS1.0/react-project-1/src/components/Profile/Profile.jsx
@@ -4,10 +4,19 @@ import s1 from './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
+
+   let posts = [
+      { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+      { id: 2, message: 'It\'s my first post 1', likesCount: 11 },  
+      { id: 3, message: 'It\'s my first post 2', likesCount: 11 },
+      { id: 4, message: 'It\'s my first post 3', likesCount: 11 },
+      { id: 5, message: 'It\'s my first post 4', likesCount: 11 }   
+   ]
+
    return (
       <div>
          <ProfileInfo />
-         <MyPosts />
+         <MyPosts posts={posts}/>
       </div>
    )
 }
