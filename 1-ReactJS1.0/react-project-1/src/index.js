0e0f7f1f4128ced162c86beecfbe9fd5ccc89734
Alex-Grh <alex_grh@mail.ru>
2023-03-09 02:24:16 +0400

    35 React JS - callback, subscribe, observer

--- 1-ReactJS1.0/react-project-1/src/index.js
+++ 1-ReactJS1.0/react-project-1/src/index.js
@@ -1,9 +1,33 @@
 import reportWebVitals from './reportWebVitals';
-import state from './components/redux/state';
-import { rerenderEntireTree } from './render';
+import state, { subscribe } from './components/redux/state';
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {addPost, updateNewPostText} from './components/redux/state';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let rerenderEntireTree = (state) => {
+root.render(
+   <React.StrictMode>
+      
+      <BrowserRouter>
+      <App appState={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+      </BrowserRouter>
+
+   </React.StrictMode>
+);
+}
 
 rerenderEntireTree (state);
+
+subscribe(rerenderEntireTree);
+
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
