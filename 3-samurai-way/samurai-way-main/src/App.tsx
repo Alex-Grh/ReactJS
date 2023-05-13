12ac45b82170d373c8883ccf6a2cf35e9ec024c0
Alex-Grh <alex_grh@mail.ru>
2023-05-14 01:13:27 +0400

    14. React JS (css modules, css-modules)

--- 3-samurai-way/samurai-way-main/src/App.tsx
+++ 3-samurai-way/samurai-way-main/src/App.tsx
@@ -1,48 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
    return (
       <div className='app-wrapper'>
-         <header className='header'>
-            <img src="https://graphicdesignbylisa.com/wp-content/uploads/generic-logo.jpg" alt="logo example" />
-         </header>
-         <nav className='nav'>
-            <div>
-               <a>Profile</a>
-               </div>
-            <div>
-            <a>Messages</a>
-            </div>
-            <div>
-            <a>News</a>
-            </div>
-            <div>
-            <a>Music</a>
-            </div>
-            <div>
-            <a>Settings</a>
-            </div>
-         </nav>
-         <div className='content'>
-            <div>
-            <img src="https://pibig.info/uploads/posts/2022-11/1669713041_4-pibig-info-p-neitralnie-oboi-na-rabochii-stol-vkontakte-4.png" alt="" />
-            </div>
-            <div>
-               ava + description
-            </div>
-            <div>
-               My post
-               <div>
-                  New post
-               </div>
-               <div>
-                  <div>Post 1</div>
-                  <div>Post 2</div>
-               </div>
-            </div>
-         </div>
-
+         <Header />
+         <Navbar />
+         <Profile />
       </div>
    );
 }
---
+++ 3-samurai-way/samurai-way-main/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Header.module.css';
+
+
+const Header = () => {
+   return  <header className={s.header}>
+   <img src="https://graphicdesignbylisa.com/wp-content/uploads/generic-logo.jpg" alt="logo example" />
+</header>
+}
+
+export default Header;
