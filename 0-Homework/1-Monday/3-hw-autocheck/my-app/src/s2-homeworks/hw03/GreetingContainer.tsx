b91deac14db6d4ee6dfe7d264c55343208ed31ac
Alex-Grh <alex_grh@mail.ru>
2023-05-22 03:29:16 +0400

    1

--- 0-Homework/1-Monday/3-hw-autocheck/my-app/src/s2-homeworks/hw03/GreetingContainer.tsx
+++ 0-Homework/1-Monday/3-hw-autocheck/my-app/src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -3,18 +3,33 @@ import Greeting from './Greeting'
 import { UserType } from './HW3'
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+   users: UserType[] // need to fix any
+   addUserCallback: (name: string) => void // need to fix any //!если не знаешь как типизировать функции пиши хотябы Function
 }
 
-export const pureAddUser = (name: any, setError: any, setName: any, addUserCallback: any) => {
-    // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+export const pureAddUser = (name: string, setError: (error: string) => void, setName: (name: string) => void, addUserCallback: (name: string) => void) => {
+
+   if (!name.trim()) {
+       setError('Ошибка! Введите имя!')
+   } else {
+       addUserCallback(name)
+       setName('')
+           }
+       // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
 }
 
-export const pureOnBlur = (name: any, setError: any) => { // если имя пустое - показать ошибку
+export const pureOnBlur = (name: string, setError: (error: string) => void) => { // если имя пустое - показать ошибку
+
+   if (!name.trim()) {
+       setError('Ошибка! Введите имя!')
+   }
 }
 
-export const pureOnEnter = (e: any, addUser: any) => { // если нажата кнопка Enter - добавить
+export const pureOnEnter = (e: React.KeyboardEvent<HTMLInputElement>, addUser: () => void) => { // если нажата кнопка Enter - добавить
+
+   if (e.key === 'Enter') {
+       return addUser()
+   }
 }
 
 // более простой и понятный для новичков
@@ -22,45 +37,50 @@ export const pureOnEnter = (e: any, addUser: any) => { // если нажата 
 
 // более современный и удобный для про :)
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
-    users,
-    addUserCallback,
+   users,
+   addUserCallback,
 }) => {
-    // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+   // деструктуризация пропсов
+   const [name, setName] = useState<string>('') // need to fix any
+   const [error, setError] = useState<string>('') // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('some name') // need to fix
-
+   const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+      const trimmedName = e.currentTarget.value.trim()
+      if (trimmedName) {
+         setName(trimmedName)
         error && setError('')
-    }
-    const addUser = () => {
-        pureAddUser(name, setError, setName, addUserCallback)
-    }
+      } else {
+        name && setName('')
+         setError('name is require!')
+      }
+   }
+   const addUser = () => {
+      pureAddUser(name, setError, setName, addUserCallback)
+   }
 
-    const onBlur = () => {
-        pureOnBlur(name, setError)
-    }
+   const onBlur = () => {
+      pureOnBlur(name, setError)
+   }
 
-    const onEnter = (e: any) => {
-        pureOnEnter(e, addUser)
-    }
+   const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+      pureOnEnter(e, addUser)
+   }
 
-    const totalUsers = 0 // need to fix
-    const lastUserName = 'some name' // need to fix
+   const totalUsers = users.length // need to fix
+   const lastUserName =  totalUsers > 0 ? users[users.length-1].name : ''// need to fix
 
-    return (
-        <Greeting
-            name={name}
-            setNameCallback={setNameCallback}
-            addUser={addUser}
-            onBlur={onBlur}
-            onEnter={onEnter}
-            error={error}
-            totalUsers={totalUsers}
-            lastUserName={lastUserName}
-        />
-    )
+   return (
+      <Greeting
+         name={name}
+         setNameCallback={setNameCallback}
+         addUser={addUser}
+         onBlur={onBlur}
+         onEnter={onEnter}
+         error={error}
+         totalUsers={totalUsers}
+         lastUserName={lastUserName}
+      />
+   )
 }
 
 export default GreetingContainer
